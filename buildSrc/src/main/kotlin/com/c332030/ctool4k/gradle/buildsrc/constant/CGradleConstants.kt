d5945a1d03cf5e2a7717c60b1dbfe1551907648d
c332030 <c332030@c332030.com>
2026-01-03 15:25:59 +0800

    feat: CGradleConstants

---
+++ buildSrc/src/main/kotlin/com/c332030/ctool4k/gradle/buildsrc/constant/CGradleConstants.kt
@@ -0,0 +1,12 @@
+package com.c332030.ctool4k.gradle.buildsrc.constant
+
+/**
+ * <p>
+ *   Description: CGradleConstants
+ * </p>
+ *
+ * @author c332030
+ * @since 2026/1/3
+ */
+
+const val JDK_VERSION = "JDK_VERSION"
